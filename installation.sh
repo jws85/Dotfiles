2da4e736e9911541fa11b32fcac9d6394ff0df0a
Justin Smith <jwsmith2spam at gmail dot com>
2018-05-05 21:16:37 -0400

    Actually run stow...

--- installation.sh
+++ installation.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Install tmux
+if [ ! -f ~/.tmux.conf ]; then
+    (cd ~/Dotfiles && stow tmux)
+fi
+
 # Install oh-my-zsh
 if [ ! -d ~/.oh-my-zsh ]; then
     git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
+    (cd ~/Dotfiles && stow zsh)
 fi
 
 # Install my emacs settings
