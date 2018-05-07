ef75cb37f04462686cbe3ef4958ad64d32b9b26c
J. W. Smith <jwsmith2spam at gmail dot com>
2018-05-07 14:39:54 -0400

    Clean up dotfile setup script a bit

--- configure-dotfiles.sh
+++ configure-dotfiles.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-# Install tmux
+# tmux
 if [ ! -f ~/.tmux.conf ]; then
     (cd ~/Dotfiles && stow tmux)
 fi
 
-# Install oh-my-zsh
+# zsh (using oh-my-zsh)
 if [ ! -d ~/.oh-my-zsh ]; then
     git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
     (cd ~/Dotfiles && stow zsh)
 fi
 
-# Install my emacs settings
+# emacs
 if [ ! -d ~/.emacs.d ]; then
     git clone https://github.com/jws85/.emacs.d.git ~/.emacs.d
 fi
 
-# Install my vim settings
+# vim
 if [ ! -d ~/.vim ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     (cd ~/Dotfiles && stow vim)
 fi
 
-# Set up git
+# git
 (cd ~/Dotfiles/ && stow git)
 
-# Set up rofi
+# rofi
 (cd ~/Dotfiles && stow rofi)
 
-# Set up gdb using https://github.com/cyrus-and/gdb-dashboard
-curl -fLo ~/.gdbinit https://git.io/.gdbinit
 
+# gdb (using https://github.com/cyrus-and/gdb-dashboard)
+curl -fLo ~/.gdbinit https://git.io/.gdbinit
