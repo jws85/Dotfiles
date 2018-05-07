63902025100bbeaa348477df9555f4404c0b93af
J. W. Smith <jwsmith2spam at gmail dot com>
2018-05-07 14:13:03 -0400

    More MATE configuration

--- configure-mate.sh
+++ configure-mate.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-## MATE Settings ###############################################################
+# It's worth noting that I go in with mate-tweak and change the layout
+# to 'Redmond' or 'Netbook' rather than the two-bar classic setup;
+# this does enough 'stuff' that it's easier to just use mate-tweak.
 
-# Set 24-hour time
+# Set up monospace font
+dconf write /org/mate/desktop/interface/monospace-font-name "'PragmataPro Mono 10.5'"
+
+# Set up mate-terminal
+dconf write /org/mate/terminal/profiles/default/use-system-font true
+dconf write /org/mate/terminal/profiles/default/use-custom-default-size true
+dconf write /org/mate/terminal/profiles/default/default-size-columns "100"
+dconf write /org/mate/terminal/profiles/default/default-size-rows "35"
+dconf write /org/mate/terminal/profiles/default/use-theme-colors false
+dconf write /org/mate/terminal/profiles/default/background-color "'#000000000000'"
+dconf write /org/mate/terminal/profiles/default/foreground-color "'#FFFFFFFFFFFF'"
+dconf write /org/mate/terminal/profiles/default/background-type "'transparent'"
+dconf write /org/mate/terminal/profiles/default/background-darkness "0.9"
+dconf write /org/mate/terminal/profiles/default/scrollbar-position "'hidden'"
+dconf write /org/mate/terminal/profiles/default/scrollback-lines "65535"
+
+# Set up virtual desktops
+dconf write /org/mate/panel/objects/workspaceswitcherapplet/prefs/display-all-workspaces true
+dconf write /org/mate/panel/objects/workspaceswitcherapplet/prefs/num-rows "1"
+dconf write /org/mate/marco/general/num-workspaces "10"
+
+# Set up time
 dconf write /org/mate/panel/objects/clockapplet/prefs/format "'24-hour'"
-
-# Show the date
 dconf write /org/mate/panel/objects/clockapplet/prefs/show-date true
 
 # Swap CapsLock and Control
@@ -31,6 +52,3 @@ action='zsh -c "emacsclient -c -n --alternate-editor=\"\""'
 binding='<Mod4>e'
 name='Emacs Client'
 SETTINGS
-
-## Startup Applications ######################################################
-
