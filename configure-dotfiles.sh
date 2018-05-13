20627d5b30cc36a88f5c6cb48e8976607163ab59
J. W. Smith <jwsmith2spam at gmail dot com>
2018-05-12 22:43:43 -0400

    Install vim-plug... w/ Ansible rather than script

--- configure-dotfiles.sh
+++ configure-dotfiles.sh
@@ -5,10 +5,5 @@ if [ ! -d ~/.oh-my-zsh ]; then
     git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
 fi
 
-# vim
-if [ ! -d ~/.vim ]; then
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
 # gdb (using https://github.com/cyrus-and/gdb-dashboard)
 curl -fLo ~/.gdbinit https://git.io/.gdbinit
