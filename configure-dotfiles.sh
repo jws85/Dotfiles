d822c3fe88b6d2780309b966e2a224c142aac978
J. W. Smith <jwsmith2spam at gmail dot com>
2018-05-12 22:35:09 -0400

    Stow dotfiles... using Ansible rather than script

--- configure-dotfiles.sh
+++ configure-dotfiles.sh
@@ -1,30 +1,14 @@
 #!/bin/bash
 
-# tmux
-if [ ! -f ~/.tmux.conf ]; then
-    (cd ~/Dotfiles && stow tmux)
-fi
-
 # zsh (using oh-my-zsh)
 if [ ! -d ~/.oh-my-zsh ]; then
     git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
-    (cd ~/Dotfiles && stow zsh)
 fi
 
 # vim
 if [ ! -d ~/.vim ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    (cd ~/Dotfiles && stow vim)
 fi
-
-# git
-(cd ~/Dotfiles/ && stow git)
-
-# rofi
-(cd ~/Dotfiles && stow rofi)
-
-# Xdefaults (likely not necessary with MATE, but hey)
-(cd ~/Dotfiles && stow xdefaults)
 
 # gdb (using https://github.com/cyrus-and/gdb-dashboard)
 curl -fLo ~/.gdbinit https://git.io/.gdbinit
