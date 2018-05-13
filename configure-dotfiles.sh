5cafaee6e2d2af4f18845a4db45d3ee8e4fa24b4
J. W. Smith <jwsmith2spam at gmail dot com>
2018-05-12 22:44:23 -0400

    Install gdbinit... with Ansible rather than script

--- configure-dotfiles.sh
+++ configure-dotfiles.sh
@@ -4,6 +4,3 @@
 if [ ! -d ~/.oh-my-zsh ]; then
     git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
 fi
-
-# gdb (using https://github.com/cyrus-and/gdb-dashboard)
-curl -fLo ~/.gdbinit https://git.io/.gdbinit
