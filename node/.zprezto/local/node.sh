d67690fef839f847d65ab07ec62b77f1a85f792e
J. W. Smith <jwsmith2spam at gmail dot com>
2020-09-08 19:16:10 -0400

    Ensure npm global install installs locally
    Note the stowing of a .zprezto/local file -- this is something I added
    in my latest zsh config commit[1] so that I can drop in language files
    (grouped together with the language's other config files) rather than
    sticking them all in my zshrc.
    
    [1] https://github.com/jws85/prezto/commit/6d529006d070faa8a535fa3fb9a013ca45be9d91

---
+++ node/.zprezto/local/node.sh
@@ -0,0 +1,5 @@
+NPM_PACKAGES="$HOME/.npm-packages"
+PATH="$NPM_PACKAGES/bin:$PATH"
+unset MANPATH
+MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
+NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
