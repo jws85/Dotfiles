853c4a609aeb1313508511017d0e20a785999a91
J. W. Smith <jwsmith2spam at gmail dot com>
2021-01-19 18:28:28 -0500

    More screwing around with fish-shell
    fish-shell is nice and easy.  problem is that the culture love love
    loves m a g i c.  (for instance, "universal" variables that don't
    require setting in the config file) i agree, m a g i c is pretty
    awesome... until you want reproducibility like i'm trying to do here!
    
    Prompts were a special pain point:
    
     - hydro is nice and minimal, but I was experiencing a bug where fzf
       wouldn't trigger on C-r, and I do that a LOT now
    
       (note, that bug could well be an id-10-t error on my part, as i
       had caught myself wiping out the fisherman config later on, which
       might cause weirdness like that)
    
     - tide has everything, but the expectation is that you should
       hand-configure using its (very easy and friendly!) wizard every
       time you re-install.  that goes against the *entire point of this b
       exercise*, sorry
    
       (can you hear my frustration yet?  i don't mean to hate on these
       projects, they're both well done!  but, AAAAAAAAAAAAAAAA)
    
     - starship requires its own binary but seems to Just Work at this
       time, so... let's go!
    
       (there's a few quibbles... it sees my ~/.composer and thinks
       the whole world's a PHP project *barf*)

--- playbooks/roles/base/files/fish/config.fish
+++ playbooks/roles/base/files/fish/config.fish
@@ -15,7 +15,13 @@ function fish_greeting
     uptime | xargs
     echo
     fortune
-    echo
+end
+
+# Helpful aliases
+function s
+    fish_title "ssh " $argv
+    set TERM xterm-256color
+    ssh $argv
 end
 
 alias x=exit
@@ -25,36 +31,5 @@ if test -f ~/.config/fish/site.fish
     source ~/.config/fish/site.fish
 end
 
-# Dracula colors, slightly edited
-set -U fish_color_normal normal
-set -U fish_color_command F8F8F2
-set -U fish_color_quote F1FA8C
-set -U fish_color_redirection 8BE9FD
-set -U fish_color_end 50FA7B
-set -U fish_color_error FFB86C
-set -U fish_color_param FF79C6
-set -U fish_color_comment 6272A4
-set -U fish_color_search_match bryellow --background=brblack
-set -U fish_color_history_current --bold
-set -U fish_color_operator 00a6b2
-set -U fish_color_escape 00a6b2
-set -U fish_color_cwd green
-set -U fish_color_cwd_root red
-set -U fish_color_valid_path --underline
-set -U fish_color_autosuggestion BD93F9
-set -U fish_color_user brgreen
-set -U fish_color_host normal
-set -U fish_color_cancel -r
-set -U fish_pager_color_completion normal
-set -U fish_pager_color_description B3A06D yellow
-set -U fish_pager_color_prefix white --bold --underline
-set -U fish_pager_color_progress brwhite --background=cyan
-set -U fish_color_selection white --bold --background=brblack
-set -U fish_color_match --background=brblue
-
-# Settings for the hydro prompt
-set --global hydro_color_prompt brmagenta
-set --global hydro_color_pwd brcyan
-set --global hydro_color_git brblue
-set --global hydro_color_error brred
-set --global hydro_color_duration brgreen
+# Use starship prompt
+starship init fish | source
