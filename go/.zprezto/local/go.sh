8cc08514d08bc1f16655edc6dc3cc65794bc1e3b
J. W. Smith <jwsmith2spam at gmail dot com>
2021-01-19 21:42:11 -0500

    s/python/go/g

--- go/.zprezto/local/go.sh
+++
@@ -1,5 +0,0 @@
-# Now that go modules are a thing, I'm moving the GOPATH to a hidden
-# location and making it non-configurable, bwahahahaha
-export PATH=$PATH:/usr/local/go/bin
-export GOPATH=$HOME/.config/go
-export PATH=$PATH:$GOPATH/bin
