265a3d8d94a615606b3b4d16b1ba5514a47c5392
J. W. Smith <jwsmith2spam at gmail dot com>
2021-03-02 13:45:50 -0500

    Need to export the GOPATH
    This was breaking commands like
    
        GO111MODULE=on go get golang.org/x/tools/gopls@latest
    
    to install the gopls LSP server.
    
    q.v. https://gist.github.com/pierre-b/8c3d2d1722f5d88533142a37ade9ecfc

--- roles/go/files/go.fish
+++ roles/go/files/go.fish
@@ -1,4 +1,4 @@
 # Great name by accident btw
 contains /usr/local/go/bin $fish_user_paths; or set -Ua fish_user_paths /usr/local/go/bin
-set GOPATH ~/.config/go
+set -x GOPATH ~/.config/go
 contains $GOPATH $fish_user_paths; or set -Ua fish_user_paths $GOPATH
