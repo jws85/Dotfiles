c0a14ca0090a3403994029f752d445c453e0e4e5
J. W. Smith <jwsmith2spam at gmail dot com>
2021-03-11 11:48:44 -0500

    Try to make copy behave more like cp
    rsync has the (IMO, annoying) behavior of making slashes on the end of source
    paths (and not destination paths) syntactically significant.  I find this
    maddening and unsuitable to something that is intended to behave like the cp
    command

--- roles/fish/files/functions/copy.fish
+++ roles/fish/files/functions/copy.fish
@@ -1,4 +1,14 @@
-# Like cp, but with progress bars
+# Like cp, but with progress bars via rsync
 function copy
-    rsync -avh --progress $argv
+    # Strip any terminal / slashes because they are syntactically significant
+    # to rsync in a way they aren't in cp:
+    # http://qdosmsq.dunbar-it.co.uk/blog/2013/02/rsync-to-slash-or-not-to-slash/
+    set newargv
+    for a in $argv
+        set -a newargv (string trim -rc '/' $a)
+    end
+    printf '%s\n' $newargv
+
+    # Run rsync
+    rsync -avh --stats --progress $newargv
 end
