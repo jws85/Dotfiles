86650791e4bf71e43a6ed86f5448e1217d7a703e
J. W. Smith <jwsmith2spam at gmail dot com>
2021-04-30 14:48:54 -0400

    phpenv.zsh for pathing; php.zsh for aliases etc
    In other words, php.zsh was copied to phpenv.zsh, and a new php.zsh
    was created for aliases (right now, running various PHP docker
    images).  phpenv.zsh should be sourced from .zshenv, and php.zsh from
    .zshrc.

--- roles/php/files/php.zsh
+++ roles/php/files/php.zsh
@@ -1,5 +1,5 @@
-type composer > /dev/null 2>&1
-
-if [ $? -eq 0 ]; then
-    export PATH=$( composer config -g home )/vendor/bin:$PATH
-fi
+# Run CLI programs against various PHP versions, using docker
+alias php8.0-cli='docker run -it --rm --name php8-lint -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:8.0-cli'
+alias php7.2-cli='docker run -it --rm --name php8-lint -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:7.2-cli'
+alias php5.6-cli='docker run -it --rm --name php8-lint -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:5.6-cli'
+alias php5.3-cli='docker run -it --rm --name php8-lint -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:5.3-cli'
