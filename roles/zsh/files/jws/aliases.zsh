2e9c155aa5b9dcf5159199fe1f0ed3546bc35a5a
J. W. Smith <jwsmith2spam at gmail dot com>
2021-05-05 16:25:34 -0400

    Alias $ to sudo

--- roles/zsh/files/jws/aliases.zsh
+++ roles/zsh/files/jws/aliases.zsh
@@ -1,6 +1,9 @@
 # Open files with 'open'
 alias open=mimeopen
 
+# Sudo with $
+alias \$=sudo
+
 # If exa (https://the.exa.website) is set up, use that for ll
 # Otherwise just use plain ol' ls
 if type exa > /dev/null; then
